4902e1428be4d60f27eb934fe6a4e0a190faaae5
Unknown <hyma.tiyyagura@gmail.com>
2017-10-11 20:44:56 +0800

    Edit StorageManager.java to follow style guide

--- src/main/java/seedu/address/storage/StorageManager.java
+++ src/main/java/seedu/address/storage/StorageManager.java
@@ -73,7 +73,8 @@ public class StorageManager extends ComponentManager implements Storage {
 
     @Override
     public void backupAddressBook(ReadOnlyAddressBook addressBook) throws IOException {
-        saveAddressBook(addressBook, addressBookStorage.getAddressBookFilePath() + "-backup.xml");
+        addressBookStorage.saveAddressBook(addressBook,
+                addressBookStorage.getAddressBookFilePath() + "-backup.xml");
     }
 
     @Override
