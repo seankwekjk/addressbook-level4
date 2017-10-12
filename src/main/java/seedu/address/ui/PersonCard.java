ead2bb4f8259e576b3b68e61c144f0b0cf10f491
mzxc152 <mzxc152@gmail.com>
2017-10-12 21:51:35 +0800

    edit code to pass style check

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -59,7 +59,6 @@ public class PersonCard extends UiPart<Region> {
             return tagColors.get(tagValue);
         }
     }
-    
 
     /**
      * Binds the individual UI elements to observe their respective {@code Person} properties
