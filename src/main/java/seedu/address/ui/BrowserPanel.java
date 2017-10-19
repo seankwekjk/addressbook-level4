9473375c31bbee971588cee7b6c9ad843a9b8772
Sean Kwek <seankwekjk@gmail.com>
2017-10-20 00:15:50 +0800

    fix for checkstyle

--- src/main/java/seedu/address/ui/BrowserPanel.java
+++ src/main/java/seedu/address/ui/BrowserPanel.java
@@ -40,10 +40,9 @@ public class BrowserPanel extends UiPart<Region> {
         registerAsAnEventHandler(this);
     }
 
-    private void loadPersonPage(ReadOnlyPerson person) {
+    private void loadPersonPage(ReadOnlyPerson per) {
         loadPage(GOOGLE_SEARCH_URL_PREFIX
-                + person.getAddress().value.replaceAll(" ", "+")
-                .replaceAll(",", "%2C"));
+                + per.getAddress().value.replaceAll(" ", "+").replaceAll(",", "%2C"));
     }
 
     public void loadPage(String url) {
