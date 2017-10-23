b834ba75506a5d62f25018fc34d70458f5675ce9
Unknown <hyma.tiyyagura@gmail.com>
2017-10-23 22:25:36 +0800

    Update MailCommand.java

--- src/main/java/seedu/address/logic/commands/MailCommand.java
+++ src/main/java/seedu/address/logic/commands/MailCommand.java
@@ -7,47 +7,67 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.person.AnyParticularContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.ReadOnlyPerson;
-import seedu.address.model.person.exceptions.DuplicatePersonException;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
- * Adds a person to the address book.
+ * Mails a person in Contags.
  */
-public class MailCommand extends UndoableCommand {
+public class MailCommand extends Command {
 
     public static final String COMMAND_WORD = "mail";
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a remark to a person in the address book. "
+    public static final String MESSAGE_SUCCESS = "Mail successfully sent.";
+    public static final String MESSAGE_FAILURE = "Mail wasn't sent. Please enter a valid mail address and valid message.";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Mails a contact in Contags.\n"
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + MAIL_RECEPIENT + " John Doe "
+            + MAIL_TITLE + " Meeting Reminder "
+            + MAIL_MESSAGE + " Meeting is at 2pm.\n";
 
-    private void sendEmail(String emailTo) throws ParseException {
-        if (emailTo.equalsIgnoreCase("")) {
-            throw new ParseException("Recipient mail is not valid. Please enter a valid mail address.");
-        }
-        if (emailTo.equalsIgnoreCase("/")) {
-            throw new ParseException("Recipient mail is not valid. Please enter a valid mail address.");
-        }
-        if (emailTo.equalsIgnoreCase(".")) {
-            throw new ParseException("Recipient mail is not valid. Please enter a valid mail address.");
-        }
-        if (emailTo.equalsIgnoreCase("\")) {
+    private final AnyParticularContainsKeywordsPredicate targetIndex;
+    private final String title;
+    private final String message;
+
+    public MailCommand(AnyParticularContainsKeywordsPredicate targetIndex, String title, String message) {
+        this.targetIndex = targetIndex;
+        this.title = title;
+        this.message = message;
+    }
+
+    private void sendMail(String sendMailTo) throws ParseException {
+        if (sendMailTo.equalsIgnoreCase(" ")) {
             throw new ParseException("Recipient mail is not valid. Please enter a valid mail address.");
         }
 
         Desktop desktop = Desktop.getDesktop();
         URI mailRecepient;
         String url = "";
+
         try {
-            url = "mailTo:" + emailTo + "&body=";
+            url = "mailRecepient:" + sendMailTo + "&body=";
             mailRecepient = new URI(url);
             desktop.mail(mailRecepient);
-        }
-        catch (URISyntaxException e) {
-            e.printStackTrace();
         }
         catch (IOException e) {
             e.printStackTrace();
         }
+        catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public CommandResult execute() {
+        try {
+            sendMail(sendMailTo);
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+            return new CommandResult(MESSAGE_FAILURE);
+        }
+        return new CommandResult(MESSAGE_SUCCESS);
     }
 
     @Override
