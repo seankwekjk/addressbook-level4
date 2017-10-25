601777f75c07427d1a81f6534a53dcba9290a398
Unknown <hyma.tiyyagura@gmail.com>
2017-10-25 18:40:26 +0800

    Add override method in AddCommandTest.java to fix bug

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -144,6 +144,12 @@ public class AddCommandTest {
         public void updateFilteredPersonList(Predicate<ReadOnlyPerson> predicate) {
             fail("This method should not be called.");
         }
+
+        @Override
+        public String updateMailRecipientList(Predicate<ReadOnlyPerson> predicate) {
+            fail("This method should not be called.");
+            return null;
+        }
     }
 
     /**
