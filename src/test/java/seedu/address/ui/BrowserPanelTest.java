f878faf3ebbd93defb1ef88832903e8cea96e067
Sean Kwek <seankwekjk@gmail.com>
2017-11-02 01:14:07 +0800

    update BrowserPanelTest for social media site testing

--- src/test/java/seedu/address/ui/BrowserPanelTest.java
+++ src/test/java/seedu/address/ui/BrowserPanelTest.java
@@ -2,9 +2,11 @@ package seedu.address.ui;
 
 import static org.junit.Assert.assertEquals;
 import static seedu.address.testutil.EventsUtil.postNow;
+import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.CARL;
 import static seedu.address.ui.BrowserPanel.DEFAULT_PAGE;
 import static seedu.address.ui.BrowserPanel.GOOGLE_SEARCH_URL_PREFIX;
+import static seedu.address.ui.BrowserPanel.SOCIAL_MEDIA_URL_PREFIX;
 import static seedu.address.ui.UiPart.FXML_FILE_FOLDER;
 
 import java.net.URL;
@@ -38,11 +40,20 @@ public class BrowserPanelTest extends GuiUnitTest {
         URL expectedDefaultPageUrl = MainApp.class.getResource(FXML_FILE_FOLDER + DEFAULT_PAGE);
         assertEquals(expectedDefaultPageUrl, browserPanelHandle.getLoadedUrl());
 
-        // associated web page of a person
+        // associated address of a person
         postNow(selectionChangedEventStub);
         URL expectedPersonUrl = new URL(GOOGLE_SEARCH_URL_PREFIX
                 + CARL.getAddress().value.replaceAll(" ", "+").replaceAll(",", "%2C"));
 
         assertEquals(expectedPersonUrl, browserPanelHandle.getLoadedUrl());
+
+        // associated social media page of a person
+        selectionChangedEventStub = new PersonPanelSelectionChangedEvent(new PersonCard(ALICE, 1));
+        BrowserPanel.setBrowserMode();
+        postNow(selectionChangedEventStub);
+        URL expectedSocialURL = new URL(SOCIAL_MEDIA_URL_PREFIX + ALICE.getSocialMedia() + "/");
+
+        assertEquals(expectedSocialURL, browserPanelHandle.getLoadedUrl());
+        BrowserPanel.setBrowserMode();
     }
 }
