0296475508c9109e66cd2c00f9fc73664cabdcaa
Unknown <hyma.tiyyagura@gmail.com>
2017-11-09 08:59:20 +0800

    Add MailCommandParserTest

---
+++ src/test/java/seedu/address/logic/parser/MailCommandParserTest.java
@@ -0,0 +1,31 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+
+import org.junit.Test;
+import seedu.address.logic.commands.MailCommand;
+
+//@@author hymss
+
+/**
+ * Test scope: similar to {@code DeleteCommandParserTest}.
+ * @see DeleteCommandParserTest
+ */
+public class MailCommandParserTest {
+
+    private MailCommandParser parser = new MailCommandParser();
+
+    @Test
+    public void parse_invalidIndex_throwsParseException() {
+        assertParseFailure(parser, "0 " ,
+                String.format(MESSAGE_INVALID_PERSON_DISPLAYED_INDEX));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                MailCommand.MESSAGE_USAGE));
+    }
+}
