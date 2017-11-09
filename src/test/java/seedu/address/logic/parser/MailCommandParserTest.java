864ccdb34cf6133b4732c8cda5c1fdfef14c1a93
Unknown <hyma.tiyyagura@gmail.com>
2017-11-09 09:46:42 +0800

    Debug mailCommandParserTest

--- src/test/java/seedu/address/logic/parser/MailCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/MailCommandParserTest.java
@@ -3,6 +3,8 @@ package seedu.address.logic.parser;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import org.junit.Test;
 
@@ -19,9 +21,8 @@ public class MailCommandParserTest {
     private MailCommandParser parser = new MailCommandParser();
 
     @Test
-    public void parse_invalidIndex_throwsParseException() {
-        assertParseFailure(parser, "0" ,
-                String.format(MESSAGE_INVALID_PERSON_DISPLAYED_INDEX));
+    public void parse_validArgs_returnsSelectCommand() {
+        assertParseSuccess(parser, "1", new MailCommand(INDEX_FIRST_PERSON));
     }
 
     @Test
