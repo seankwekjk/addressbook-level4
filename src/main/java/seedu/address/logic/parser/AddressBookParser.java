416375609bd1e83ef3d9b3dfd6484ba69ae98100
Unknown <hyma.tiyyagura@gmail.com>
2017-11-11 23:23:21 +0800

    Fix checkstyle issue

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -86,7 +86,7 @@ public class AddressBookParser {
             return new ListCommand();
 
         case ListBirthdayCommand.COMMAND_WORD:
-            case ListBirthdayCommand.COMMAND_ALIAS:
+        case ListBirthdayCommand.COMMAND_ALIAS:
             return new ListBirthdayCommand();
 
         case SmsCommand.COMMAND_WORD:
