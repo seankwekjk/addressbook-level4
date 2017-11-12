48e39538996aae7dbdaa404299169e4ac270b948
Unknown <hyma.tiyyagura@gmail.com>
2017-11-12 11:21:11 +0800

    Add listBirthdayCommandTest

---
+++ src/test/java/seedu/address/logic/commands/ListBirthdayCommandTest.java
@@ -0,0 +1,44 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.showFirstPersonOnly;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.Before;
+import org.junit.Test;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.UndoRedoStack;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+//@@author hymss
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for ListCommand.
+ */
+public class ListBirthdayCommandTest {
+
+    private Model model;
+    private Model expectedModel;
+    private ListBirthdayCommand listBirthdayCommand;
+
+    @Before
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+
+        listBirthdayCommand = new ListBirthdayCommand();
+        listBirthdayCommand.setData(model, new CommandHistory(), new UndoRedoStack());
+    }
+
+    @Test
+    public void execute_listIsNotFiltered_showsSameList() {
+        assertCommandSuccess(listBirthdayCommand, model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_listIsFiltered_showsEverything() {
+        showFirstPersonOnly(model);
+        assertCommandSuccess(listBirthdayCommand, model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+}
